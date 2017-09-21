fcbfdc8cab3e219f26b23fba86de199416008f85
Lithry <facundosargiotti@gmail.com>
2017-09-21 17:30:45 -0300

    Save Vars

---
+++ Assets/Scripts/DebugConsoleView.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugConsoleView : MonoBehaviour
+{
+	public Text ConsoleTxt;
+	public InputField InputConsole;
+	private Dictionary<string, string> vars = new Dictionary<string, string>();
+
+	// Use this for initialization
+	void Start () {
+		InputConsole.onEndEdit.AddListener(OnEndEdit);
+		DebugConsole.Instance.RegisterCommand("Log", LogCommand);
+		DebugConsole.Instance.RegisterCommand("CreateObject", CreateObjectCommand);
+		DebugConsole.Instance.RegisterCommand("SaveVar", SaveVarCommand);
+	}
+
+	void OnEndEdit(string val)
+	{
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			Debug.Log(val);
+			if (!string.IsNullOrEmpty(val))
+			{
+				ExecuteCommand(val);
+				InputConsole.text = "";
+			}
+		}
+	}
+
+	void ExecuteCommand(string str)
+	{
+		string log = DebugConsole.Instance.ExecuteCommand(str);
+		Log(log);
+	}
+
+	void Log(string str)
+	{
+		ConsoleTxt.text += "\n" + str;
+	}
+
+	string LogCommand(params string[] args)
+	{
+		string str = "";
+
+		if (args != null)
+		{
+			for (int i = 0; i < args.Length; i++)
+			{
+				str += args[i] + "\n";
+			}
+
+			return str;
+		}
+
+		return "No arguments...";
+	}
+
+	string CreateObjectCommand(params string[] args)
+	{
+		if (args != null && args.Length > 0)
+		{
+			GameObject prefab = Resources.Load<GameObject>(args[0]);
+			if (prefab)
+			{
+				GameObject.Instantiate(prefab);
+				return "OK";
+			}
+			else
+				return "Error loading \"" + args[0] + "\" prefab.";
+		}
+
+
+		return "No arguments...";
+	}
+
+	string SaveVarCommand(params string[] args){
+		if (args != null && args.Length == 2){
+			if (args[1].Length == 1){					//
+				if (char.IsLetter(args[1][0])){			// if arg is a var
+					if (vars.ContainsKey(args[1])){		//
+						if (vars.ContainsKey(args[0])){
+							vars[args[0]] = vars[args[1]];
+							return "Var " + args[0] + " has now the value of var " + args[1] + " (\"" + vars[args[1]] + "\")";
+						}
+						else{
+							vars.Add(args[0], args[1]);
+							return "New Var " + args[0] + " has the value of var " + args[1] + " (\"" + vars[args[1]] + "\")";
+						}
+					}
+				}
+			}
+			
+			if (vars.ContainsKey(args[0])){
+				vars[args[0]] = args[1];
+				return "Var " + args[0] + " has now the value of " + "\"" + args[1] + "\"";
+			}
+			else{
+				vars.Add(args[0], args[1]);
+				return "New Var " + args[0] + " has the value of " + "\"" + args[1] + "\"";
+			}
+		}
+
+		return "Error in arguments...";
+	}
+}
---
+++ Assets/Scripts/Parser.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+	public string CommandName;
+	public List<string> Args;
+}
+
+public class Parser 
+{
+	enum State
+	{
+		CommandName,
+		Arguments
+	}
+
+	Tokenizer tokenizer = new Tokenizer();
+	Command command = new Command();
+	State state = State.CommandName;
+
+	void Reset()
+	{
+		command.CommandName = null;
+		command.Args = new List<string>();
+		state = State.CommandName;
+	}
+
+	public bool Parse(string str, out Command cmd)
+	{
+		Reset();
+		tokenizer.Start(str);
+
+		cmd = command;
+
+		Tokenizer.Token currentToken = tokenizer.GetNextToken();
+	
+		if (currentToken.Type == Tokenizer.TokenType.Empty)
+		{
+			return false;
+		}
+		
+		while (currentToken.Type != Tokenizer.TokenType.EOL && currentToken.Type != Tokenizer.TokenType.Unknown)
+		{
+			switch(state)
+			{
+				case State.CommandName:
+					if (currentToken.Type != Tokenizer.TokenType.Ident)
+						return false;
+
+					command.CommandName = currentToken.Lexeme; // Take current lexeme as command name
+					currentToken = tokenizer.GetNextToken(); // skip to next token
+					
+					if (currentToken.Type == Tokenizer.TokenType.OpenParent)
+					{
+						state = State.Arguments;
+						currentToken = tokenizer.GetNextToken(); // Skip parenthesis
+					}
+					else if (currentToken.Type == Tokenizer.TokenType.Equal)
+					{
+						if (cmd.CommandName.Length < 1 || cmd.CommandName.Length > 1)
+							return false;
+						
+						cmd.Args.Add(cmd.CommandName);
+						cmd.CommandName = "SaveVar";
+						state = State.Arguments;
+						currentToken = tokenizer.GetNextToken(); // Skip equal
+					}
+					else if (currentToken.Type == Tokenizer.TokenType.EOL)
+						return true;
+					else 
+						return false;
+				break;
+
+				case State.Arguments:
+					if (currentToken.Type == Tokenizer.TokenType.Number || currentToken.Type == Tokenizer.TokenType.String)
+					{
+						command.Args.Add(currentToken.Lexeme);
+						currentToken = tokenizer.GetNextToken();
+						
+						if (currentToken.Type == Tokenizer.TokenType.Comma)
+							currentToken = tokenizer.GetNextToken(); // skip comma
+						else if (currentToken.Type == Tokenizer.TokenType.CloseParent)
+						{
+							currentToken = tokenizer.GetNextToken();
+
+							if (currentToken.Type != Tokenizer.TokenType.EOL)
+								return false;
+							else
+								return true;
+						}
+						else if (currentToken.Type == Tokenizer.TokenType.EOL)
+						{
+							if (cmd.CommandName == "SaveVar")
+								return true;
+							else
+								return false;
+						}
+						else
+							return false; // Syntax error! 
+					}
+					else if (currentToken.Type == Tokenizer.TokenType.CloseParent)
+					{
+						currentToken = tokenizer.GetNextToken();
+
+						if (currentToken.Type != Tokenizer.TokenType.EOL)
+							return false; // Syntax error! 
+						else
+							return true;
+					}
+					else if (currentToken.Type == Tokenizer.TokenType.Ident && currentToken.Lexeme.Length == 1 && cmd.CommandName == "SaveVar"){
+						command.Args.Add(currentToken.Lexeme);
+						currentToken = tokenizer.GetNextToken();
+
+						if (currentToken.Type == Tokenizer.TokenType.EOL)
+						{
+							return true;
+						}
+						else
+							return false;
+					}
+					else
+					{
+						return false; // Syntax error!
+					}
+				break;
+			}
+		}
+
+		return true;
+	}
+
+}
